53301b04b37ce7cf506ebb152d60b695be75a412
Moein Rahmani <moeinrahmani951@gmail.com>
2022-06-12 16:54:08 +0430

    create & edit page for permission & role

--- app/Http/Controllers/Admin/RoleController.php
+++ app/Http/Controllers/Admin/RoleController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers\Admin;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use Spatie\Permission\Models\Permission;
 use Spatie\Permission\Models\Role;
 
 class RoleController extends Controller
@@ -12,5 +13,53 @@ class RoleController extends Controller
     public function index(){
         $roles = Role::all();
         return view('admin.roles.index',compact('roles'));
+    }
+
+    public function create(){
+        return view('admin.roles.create');
+    }
+
+    public function store(Request $request)
+    {
+        $validated = $request->validate(['name' => ['required', 'min:3']]);
+        Role::create($validated);
+        return to_route('admin.roles.index')->with('message', 'نقش کاربری با موفقیت ایجاد شد.');
+    }
+
+    public function edit(Role $role)
+    {
+        $permissions = Permission::all();
+        return view('admin.roles.edit', compact('role', 'permissions'));
+    }
+
+    public function update(Request $request, Role $role)
+    {
+        $validated = $request->validate(['name' => ['required', 'min:3']]);
+        $role->update($validated);
+        return to_route('admin.roles.index')->with('message', 'نقش کاربری با موفقیت ویرایش شد.');
+    }
+
+    public function destroy(Role $role)
+    {
+        $role->delete();
+        return back()->with('message', 'نقش کاربری حذف شد.');
+    }
+
+    public function givePermission(Request $request, Role $role)
+    {
+        if($role->hasPermissionTo($request->permission)){
+            return back()->with('message', 'Permission exists.');
+        }
+        $role->givePermissionTo($request->permission);
+        return back()->with('message', 'Permission added.');
+    }
+
+    public function revokePermission(Role $role, Permission $permission)
+    {
+        if($role->hasPermissionTo($permission)){
+            $role->revokePermissionTo($permission);
+            return back()->with('message', 'Permission revoked.');
+        }
+        return back()->with('message', 'Permission not exists.');
     }
 }
